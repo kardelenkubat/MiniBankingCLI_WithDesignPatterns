8fb029518304e5800d960abbd0642ad56a52c154
kkubat <kkubat@N401955>
2026-05-07 10:48:06 +0300

    Add project files.

---
+++ MiniBankingCLI/Domain/Enums/AccountType.cs
@@ -0,0 +1,13 @@
+namespace MiniBankingCLI.Domain.Enums
+{
+    public class AccountType
+    {
+        public enum Deposit;
+        public enum WithDrawal;
+        public enum Transfer;
+        public enum Interest;
+        public enum Commission;
+
+
+    }
+}
